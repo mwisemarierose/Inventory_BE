17c4f3f44df85d0f44e3c536aa076bd94b725570
mwisemarierose <mwisemamarierose@gmail.com>
2024-04-23 11:50:36 +0200

    fix id error in deployment

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended:  false }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your frontend origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+    next();
+});
 
 
 //routes middleware
