fc8af1e26666a3174aa8d5971d9377404d10c57d
mwisemarierose <mwisemamarierose@gmail.com>
2024-04-23 12:07:46 +0200

    fix id error in deployment

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended:  false }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://kmdinventory.netlify.app'); // Replace with your frontend origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
-    next();
-});
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "https://kmdinventory.netlify.app"],
+      credentials: true,
+    })
+  );
 
 
 //routes middleware
